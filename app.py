0e474dc02ee9ea5b64da0dfa40e84e70bdbc3095
maaaaad007-source <maaaaad007@gmail.com>
2026-08-06 18:06:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if st.button("Search Personnel"):
                 emails = data.get("emails", [])
 
                 if emails:
-                    target_roles = ["ceo", "chief executive", "founder", "director", "human resource", "user experience", "design", "ux", "product", "hr", "people", "talent"]
+                    target_roles = ["ceo", "chief executive", "founder", "partner", "director", "human resource", "user experience", "design", "ux", "product", "hr", "people", "talent"]
                     filtered_people = []
 
                     for person in emails:
